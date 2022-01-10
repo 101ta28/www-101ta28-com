b25614720f6cdfb6fa36aefdd72722bb604c0d5e
101ta28 <workta28@gmail.com>
2022-01-10 18:45:01 +0900

    change menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,9 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          101ta28.com
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -50,47 +49,25 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
+    title: 'Profile',
+    icon: 'person',
     link: 'https://quasar.dev'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
+    title: 'Skill',
+    icon: 'lightbulb',
     link: 'https://chat.quasar.dev'
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: 'Works',
+    icon: 'list',
+    link: 'https://github.com/quasarframework'
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
+    title: 'Contact',
     icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    link: 'https://github.com/quasarframework'
   },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
 ];
 
 import { defineComponent, ref } from 'vue'
