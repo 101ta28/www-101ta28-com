6c864d25092326d4bb14508178d16f794a1aedd3
Tatsuya Imai <workta28@gmail.com>
2022-01-17 16:24:03 +0900

    write works page

--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -1,13 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>This is Works</h1>
+  <q-page class="text-center">
+    <h2>
+      <q-icon name="code" />
+      Works
+    </h2>
+    <q-separator spaced inset />
+    <h4>私が開発に携わったものです。</h4>
+    <q-card flat bordered class="my-card">
+      <img src="img/ar_paper.jpg"/>
+      <q-card-section>
+        <div class="text-overline text-orange-9">2019年頃</div>
+        <div class="text-h6">室内情報可視化の研究</div>
+      </q-card-section>
+
+      <q-separator inset />
+
+      <q-card-section class="q-pt-none">
+        IoTと拡張現実技術を組み合わせた、室内情報可視化の研究を行っていました。
+      </q-card-section>
+
+      <q-separator inset />
+
+      <q-btn label="別タブで画像を開く" color="primary" />
+    </q-card>
   </q-page>
 </template>
 
+<style lang="scss">
+  .my-card {
+    width: 100%;
+    max-width: 250px;
+  }
+</style>
+
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  name: "PageIndex",
+});
 </script>
