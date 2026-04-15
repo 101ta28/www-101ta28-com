e72df6dc032af5dd5b1c616e969a33ab8f148bfd
101ta28 <workta28@gmail.com>
2026-04-16 06:51:07 +0900

    update: update packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,55 @@
-export { default } from 'eslint-config-vuetify'
+import importPlugin from 'eslint-plugin-import';
+import n from 'eslint-plugin-n';
+import promise from 'eslint-plugin-promise';
+import vue from 'eslint-plugin-vue';
+
+export default [
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      '.wrangler/**',
+      'components.d.ts',
+      'typed-router.d.ts',
+    ],
+  },
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+        process: 'readonly',
+        URL: 'readonly',
+        window: 'readonly',
+      },
+    },
+    plugins: {
+      import: importPlugin,
+      n,
+      promise,
+    },
+    rules: {
+      'comma-dangle': ['error', 'always-multiline'],
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'object-curly-spacing': ['error', 'always'],
+      'space-before-function-paren': ['error', 'never'],
+      semi: ['error', 'always'],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'n/no-missing-import': 'off',
+      'n/no-unsupported-features/es-syntax': 'off',
+      'promise/always-return': 'off',
+    },
+  },
+  {
+    files: ['src/pages/**/*.vue'],
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+];
